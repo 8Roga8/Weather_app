6b08e6367762eb84d3bad0e333a1c71396a16789
William Rojas <rojas.master@hotmail.com>
2024-03-07 15:56:26 -0400

    Entregable_2

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div>
+      <div className='weather_Loader1'></div>
+    <h1 className='weather_Loader2'>loading...</h1>
+    </div>
+  )
+}
+
+export default Loader
